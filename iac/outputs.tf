38873b62f84a099d8319344c43b773a5080eba87
ankoay feno <ankoayfeno@gmail.com>
2025-10-30 11:54:02 +0300

    Refactor Terraform configuration: modularize VPC, IAM, and EKS setups; streamline outputs and variables

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,12 +1,6 @@
-# outputs.tf - Fichier principal
 output "cluster_id" {
   description = "ID du cluster EKS"
   value       = module.eks.cluster_id
-}
-
-output "cluster_arn" {
-  description = "ARN du cluster EKS"
-  value       = module.eks.cluster_arn
 }
 
 output "cluster_endpoint" {
@@ -14,32 +8,16 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Certificate authority data pour le cluster"
-  value       = module.eks.cluster_certificate_authority_data
-}
-
-output "node_group_arn" {
-  description = "ARN du node group"
-  value       = module.eks.node_group_arn
-}
-
-output "node_group_status" {
-  description = "Statut du node group"
-  value       = module.eks.node_group_status
-}
-
-output "configure_kubectl" {
-  description = "Commande pour configurer kubectl"
-  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${var.cluster_name}"
+output "cluster_name" {
+  description = "Nom du cluster EKS"
+  value       = module.eks.cluster_name
 }
 
 output "vpc_id" {
   description = "ID du VPC"
-  value       = module.networking.vpc_id
+  value       = module.vpc.vpc_id
 }
 
-output "cluster_autoscaler_role_arn" {
-  description = "ARN du rôle IAM du Cluster Autoscaler"
-  value       = module.autoscaling.cluster_autoscaler_role_arn
+output "synchro_kube_config" {
+  value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,30 +1,17 @@
-# variables.tf
 variable "aws_region" {
-  description = "AWS region"
+  description = "Région AWS"
   type        = string
-  default     = "eu-west-3"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "Nom du cluster EKS"
   type        = string
   default     = "my-eks-cluster"
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version"
+variable "environment" {
+  description = "Environnement (dev, staging, prod)"
   type        = string
-  default     = "1.34"
-}
-
-variable "addon_version" {
-  description = "Version of the EKS addon"
-  type        = string
-  default     = ""
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  default     = "dev"
 }
